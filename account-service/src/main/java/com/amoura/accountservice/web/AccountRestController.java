008df09627274b550a5f966e7cb5d0ee50de0349
AMOURA Mohand-rachid <m-amoura@enageo.com>
2024-03-09 14:50:16 +0100

    ajouter la config pour la découverte automatique des microservice

--- account-service/src/main/java/com/amoura/accountservice/web/AccountRestController.java
+++ account-service/src/main/java/com/amoura/accountservice/web/AccountRestController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/v1")
+@RequestMapping
 public class AccountRestController {
 
     private final BanckAccountRepository banckAccountRepository;
--- gateway-service/src/main/java/com/amoura/gatewayservice/GatewayServiceApplication.java
+++ gateway-service/src/main/java/com/amoura/gatewayservice/GatewayServiceApplication.java
@@ -2,6 +2,10 @@ package com.amoura.gatewayservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.ReactiveDiscoveryClient;
+import org.springframework.cloud.gateway.discovery.DiscoveryClientRouteDefinitionLocator;
+import org.springframework.cloud.gateway.discovery.DiscoveryLocatorProperties;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class GatewayServiceApplication {
@@ -9,5 +13,18 @@ public class GatewayServiceApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(GatewayServiceApplication.class, args);
 	}
+
+	/**
+	 * Crée un bean pour la découverte dynamique de routes dans Spring Cloud Gateway.
+	 *
+	 * @param rdc Client ReactiveDiscoveryClient permettant d'interagir avec le registre de services.
+	 * @param ldp Propriétés DiscoveryLocatorProperties pour la configuration de la découverte de routes.
+	 * @return Un DiscoveryClientRouteDefinitionLocator pour la gestion dynamique des routes.
+	 */
+	@Bean
+	public DiscoveryClientRouteDefinitionLocator locator(ReactiveDiscoveryClient rdc, DiscoveryLocatorProperties ldp) {
+		return new DiscoveryClientRouteDefinitionLocator(rdc, ldp);
+	}
+
 
 }
